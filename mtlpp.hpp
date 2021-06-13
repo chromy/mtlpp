59ae31356fc16fa2f97fef496013f0856e37f89c
koya <gnndr36@gmail.com>
2021-06-13 15:30:51 +0900

    Ran amalgamation.sh

--- mtlpp.hpp
+++ mtlpp.hpp
@@ -2785,30 +2785,6 @@ namespace mtlpp
 //////////////////////////////////////
 // FILE: clear_color.hpp
 //////////////////////////////////////
-// #pragma once
-
-// #include "defines.hpp"
-// #include "ns.hpp"
-
-namespace mtlpp
-{
-    class ClearColor
-    {
-        double red;
-        double green;
-        double blue;
-        double alpha;
-
-        explicit ClearColor(double red, double green, double blue, double alpha)
-         : red(red), green(green), blue(blue), alpha(alpha)
-        {
-        }
-
-        explicit ClearColor() : ClearColor(0, 0, 0, 0)
-        {
-        }
-    };
-}
 
 //////////////////////////////////////
 // FILE: mtlpp.hpp
